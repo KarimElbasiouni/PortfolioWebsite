149f6c12b8f50c2e28feb06b60d1944a87e397ee
KarimElbasiouni <elbasiounikarim@gmail.com>
2025-02-17 21:59:33 -0500

    Define page components

---
+++ web-portfolio/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Meteors } from './ui/meteors'
+
+const Hero = () => {
+  return (
+    <div className = "">
+        <Meteors number={50} className ="" />
+    </div>
+  )
+}
+
+export default Hero
